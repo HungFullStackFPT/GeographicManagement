c7b89aabbd7f0dca8318b842af0f52f8f37d286a
Nguyễn Văn Việt Hưng <viethungnguyen2004@gmail.com>
2024-01-26 11:18:08 +0700

    Initial commit

---
+++ src/geographicmanagement/GeographicManagement.java
@@ -0,0 +1,11 @@
+
+package geographicmanagement;
+
+import controller.ManageEastAsiaCountries;
+
+public class GeographicManagement {
+    public static void main(String[] args) {
+        new ManageEastAsiaCountries().run();
+    }
+    
+}
